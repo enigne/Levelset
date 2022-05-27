7ccfee2657f38d925edd3c4d2a7b6273b82403b1
Cheng Gong <enigne@gmail.com>
2022-05-27 05:03:40 -0400

    stab test with 0.5T advance and retreat

--- experiments.m
+++ experiments.m
@@ -3,6 +3,7 @@ close all
 
 today = datestr(date(), 'yyyymmdd');
 
+finalTime = 25;
 reinit = [0, 1, 10, 50, 100];
 stablization = [1,2,5];
 steps = [6];
@@ -10,7 +11,8 @@ steps = [6];
 for i = 1:length(reinit)
 	for l = 1:length(stablization)
 		savePath = [today, '_LevelsetTest', '_stab', num2str(stablization(l)), '_reinit', num2str(reinit(i))];
-		md = runme('steps', steps, ...
+		md = runme('steps', steps,...
+			'finalTime', finalTime,...
 			'levelset stabilization', stablization(l),...
 			'levelset reinitialize', reinit(i),...
 			'savePath', [savePath]);
