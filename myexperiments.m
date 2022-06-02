8b68797f4d99243440a2fb7548605c5a73a36ecf
Cheng Gong <enigne@gmail.com>
2022-06-02 04:45:30 -0400

    rerun full domain test but with transient settings of the iniital vel

--- myexperiments.m
+++ myexperiments.m
@@ -4,13 +4,13 @@ close all
 today = datestr(date(), 'yyyymmdd');
 
 finalTime = 25;
-reinit = [0];%, 1, 10, 50, 100];
-stablization = [1];%,2,5];
+reinit = [0, 1, 10, 50, 100];
+stablization = [1,2,5];
 steps = [8];
 
 for i = 1:length(reinit)
 	for l = 1:length(stablization)
-		savePath = [today, '_LevelsetTest_zeroSide', '_stab', num2str(stablization(l)), '_reinit', num2str(reinit(i))];
+		savePath = [today, '_LevelsetTest', '_stab', num2str(stablization(l)), '_reinit', num2str(reinit(i))];
 		md = runme('steps', steps,...
 			'finalTime', finalTime,...
 			'levelset stabilization', stablization(l),...
