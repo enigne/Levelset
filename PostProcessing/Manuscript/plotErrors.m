ea79e84f4d2e4cd4f046770d286305860f79d707
Cheng Gong <enigne@gmail.com>
2023-02-15 14:14:02 -0500

    try to plot for the levelset paper

--- PostProcessing/Manuscript/plotErrors.m
+++ PostProcessing/Manuscript/plotErrors.m
@@ -6,12 +6,12 @@ projPath = ['/totten_1/chenggong/', glacier, '/'];
 figNamePrefix = [projPath, 'PostProcessing/Figures/'];
 saveflag = 1;
 
-finalTime = 5;
-Ids = [102, 112, 122, 132];
-figs = {'rectangle', 'rectangle_side0', 'semicircle', 'semicircle_side0'};
-linestyles = {'-', ':'};
+finalTime = 50;
+Ids = [304, 404];
+figs = {'semicircle_uniform', 'rectangle_uniform'};
+linestyles = {'-', ':', '--'};
 colorstyle = {'#0072BD', '#D95319', '#EDB120'};
-Nlines = 2;
+Nlines = 3;
 % start the loop {{{
 for iid = 1:length(Ids)
 	Id = Ids(iid);
@@ -26,30 +26,30 @@ for iid = 1:length(Ids)
 	nsub = 2;
 	%}}}
 	%% Average behaviors {{{
-	figure('position',[0,300,400,300])
+	figure('position',[0,1000,800,600])
 
 	for i = 1: Ntrans
 		time = transData{i}.time;
 		% number of misfit elements no abs
 		subplot(nsub,1,1)
-		plot(transData{i}.time, transData{i}.total_misfit/1e6, 'LineWidth', 2, 'LineStyle', linestyles{mod((i-1), Nlines)+1},'Color', colorstyle{fix((i-1)/ Nlines)+1});
+		plot(transData{i}.time_misfit, transData{i}.total_misfit/1e6, 'LineWidth', 2, 'LineStyle', linestyles{fix((i-1)/Nlines)+1},'Color', colorstyle{mod((i-1), Nlines)+1});
 		hold on
 
 		subplot(nsub,1,2)
-		plot(transData{i}.time, transData{i}.total_abs_misfit/1e6, 'LineWidth', 2, 'LineStyle', linestyles{mod((i-1), Nlines)+1},'Color', colorstyle{fix((i-1)/ Nlines)+1});
+		plot(transData{i}.time_misfit, transData{i}.total_abs_misfit/1e6, 'LineWidth', 2, 'LineStyle', linestyles{fix((i-1)/Nlines)+1},'Color', colorstyle{mod((i-1), Nlines)+1});
 		hold on
 	end
 	subplot(nsub, 1, 1);
 	title('total misfit area')
 	xlim([0, finalTime])
-	ylim([-4, 4])
+	ylim([-25, 25])
 	xlabel('Time (a)')
 	ylabel('Misfit area (km$^2$)', 'Interpreter', 'latex')
 
 	subplot(nsub, 1, 2);
 	title('total absolute misfit area')
 	xlim([0, finalTime])
-	ylim([0, 4])
+	ylim([0, 25])
 	xlabel('Time (a)')
 	ylabel('Misfit area (km$^2$)', 'Interpreter', 'latex')
 
