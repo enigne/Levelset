8fe1e3506096a80c40f100bf690362fb96b59b55
Cheng Gong <enigne@gmail.com>
2023-02-16 14:07:24 -0500

    plot error curves

--- PostProcessing/Manuscript/plotErrors.m
+++ PostProcessing/Manuscript/plotErrors.m
@@ -3,12 +3,19 @@ close all
 
 glacier = 'Levelset';
 projPath = ['/totten_1/chenggong/', glacier, '/'];
-figNamePrefix = [projPath, 'PostProcessing/Figures/'];
+figNamePrefix = [pwd(), '/Figures/'];
 saveflag = 1;
 
 finalTime = 50;
-Ids = [304, 404];
-figs = {'semicircle_uniform', 'rectangle_uniform'};
+Ids = [304, 404, 301, 302, 303, 314];
+figs = {'semicircle_uniform_1000', 'rectangle_uniform_1000', 'semicircle_parabola_1000', 'semicircle_trangle_1000', 'semicircle_gaussian_1000', 'semicircle_uniform_5000'};
+figtitles = {'Semicircle, uniform $v_0=1000$ m/a', 
+				'Straight line, uniform $v_0=1000$ m/a', 
+				'Semicircle, parabola $v_0=1000$ m/a', 
+				'Semicircle, triangle $v_0=1000$ m/a', 
+				'Semicircle, gaussian $v_0=1000$ m/a', 
+				'Semicircle, uniform $v_0=5000$ m/a', 
+				};
 linestyles = {'-', ':', '--'};
 colorstyle = {'#0072BD', '#D95319', '#EDB120'};
 Nlines = 3;
@@ -23,37 +30,36 @@ for iid = 1:length(Ids)
 	% Load simulations from transient.mat
 	transData = loadData(folderList, 'levelset', [projPath, 'Models/']);
 	Ntrans = length(transData);
-	nsub = 2;
 	%}}}
 	%% Average behaviors {{{
-	figure('position',[0,1000,800,600])
+	figure('position',[0,1000,600,200])
 
 	for i = 1: Ntrans
 		time = transData{i}.time;
 		% number of misfit elements no abs
-		subplot(nsub,1,1)
-		plot(transData{i}.time_misfit, transData{i}.total_misfit/1e6, 'LineWidth', 2, 'LineStyle', linestyles{fix((i-1)/Nlines)+1},'Color', colorstyle{mod((i-1), Nlines)+1});
-		hold on
+		%		subplot(nsub,1,1)
+		%		plot(transData{i}.time_misfit, transData{i}.total_misfit/1e6, 'LineWidth', 2, 'LineStyle', linestyles{fix((i-1)/Nlines)+1},'Color', colorstyle{mod((i-1), Nlines)+1});
+		%		hold on
 
-		subplot(nsub,1,2)
+		%		subplot(nsub,1,2)
 		plot(transData{i}.time_misfit, transData{i}.total_abs_misfit/1e6, 'LineWidth', 2, 'LineStyle', linestyles{fix((i-1)/Nlines)+1},'Color', colorstyle{mod((i-1), Nlines)+1});
 		hold on
 	end
-	subplot(nsub, 1, 1);
-	title('total misfit area')
-	xlim([0, finalTime])
-	ylim([-25, 25])
-	xlabel('Time (a)')
-	ylabel('Misfit area (km$^2$)', 'Interpreter', 'latex')
+	%	subplot(nsub, 1, 1);
+	%	title('total misfit area')
+	%	xlim([0, finalTime])
+	%	ylim([-25, 25])
+	%	xlabel('Time (a)')
+	%	ylabel('Misfit area (km$^2$)', 'Interpreter', 'latex')
 
-	subplot(nsub, 1, 2);
-	title('total absolute misfit area')
+	%	subplot(nsub, 1, 2);
+	title(figtitles{iid}, 'Interpreter', 'latex')
 	xlim([0, finalTime])
 	ylim([0, 25])
 	xlabel('Time (a)')
-	ylabel('Misfit area (km$^2$)', 'Interpreter', 'latex')
+	ylabel('Absolute misfit area (km$^2$)', 'Interpreter', 'latex')
 
-%	legend(nameList, 'Interpreter', 'latex','Orientation','horizontal','Location','bestoutside')
+	%	legend(nameList, 'Interpreter', 'latex','Orientation','vertical','Location','eastoutside')
 	set(gcf,'color','w');
 	if saveflag
 		export_fig([figName, '.pdf'])
--- PostProcessing/Manuscript/plotLegend.m
+++ PostProcessing/Manuscript/plotLegend.m
@@ -3,7 +3,7 @@ close all
 
 glacier = 'Levelset';
 projPath = ['/totten_1/chenggong/', glacier, '/'];
-figNamePrefix = [projPath, 'PostProcessing/Figures/'];
+figNamePrefix = [pwd(), '/Figures/'];
 figName = 'legend';
 saveflag = 1;
 
@@ -12,7 +12,7 @@ colorstyle = {'#0072BD', '#D95319', '#EDB120'};
 nameList = {'AD', 'UW', 'SUPG', 'reinit=1', 'reinit=10', 'reinit=100'};
 Nlines = 3;
 % start the loop {{{
-figure('position',[0,1000,800,300])
+figure('position',[0,1000,600,200])
 hold on
 for i = 1:length(colorstyle)
 	plot([1:10],[1:10], 'LineWidth', 2, 'Color', colorstyle{i}, 'LineStyle', '-')
@@ -23,6 +23,6 @@ end %}}}
 legend(nameList, 'Interpreter', 'latex','Orientation','horizontal','Location','northoutside')
 set(gcf,'color','w');
 if saveflag
-	export_fig([figName, '.pdf'])
+	export_fig([figNamePrefix, figName, '.pdf'])
 end
 %}}}
