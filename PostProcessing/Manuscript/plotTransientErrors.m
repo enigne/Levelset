fb2c167340ea92f0261bb921a68508c10a18c826
Cheng Gong <enigne@gmail.com>
2023-05-02 14:50:56 -0400

    plot detailed errors

--- PostProcessing/Manuscript/plotTransientErrors.m
+++ PostProcessing/Manuscript/plotTransientErrors.m
@@ -3,73 +3,36 @@ close all
 
 glacier = 'Levelset';
 projPath = ['/totten_1/chenggong/', glacier, '/'];
-figNamePrefix = [pwd(), '/Figures/'];
-saveflag = 0;
-folderList = {'20230307_LS_circle_uniform_vx1000_stab1_reinit1/'};
-id = 1;
-t = [2, 10, 30, 50];
+figNamePrefix = [projPath, 'PostProcessing/Manuscript/Figures/'];
+saveflag = 1;
+st = 1; 
+re = 1;
+folderList = {['20230307_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
 % Load data {{{
 % load model
 md = loadRefMd();
 % load data
 transData = loadData(folderList, 'levelset', [projPath, 'Models/']);
 NT = transData{1}.NT;
-nplot = numel(t);
 %}}}
+% plot {{{
+id = 40;
+t = [2, 10, 25, 50];
+nplot = numel(t);
 for i = 1:length(transData)
-	figure('position',[0,1000,800,1000])
 	for p = 1:nplot
-		dist = 0.5*(sign(transData{i}.ice_levelset(:,id+(t(p)-1)*NT)) - sign(transData{i}.analytical_levelset(:,id)));
-		plotmodel(md, 'data', dist, 'subplot', [2,ceil(nplot/2),p], 'figure', i, 'caxis', [-1,1])
-	end
+		tid = id+(t(p)-1)*NT;
+		dist = 0.5*(sign(transData{i}.ice_levelset(:,tid)) - sign(transData{i}.analytical_levelset(:,id)));
+		plotmodel(md, 'data', dist, 'figure', p, 'caxis', [-1,1], 'title', ['time=', num2str(transData{i}.time(tid))], 'figposition', [0,1000,500,400])
+		xlabel('x (m)')
+		ylabel('y (m)')
 
-end
-return
-
-
-% start the loop {{{
-for iid = 1:1
-	Id = Ids(iid);
-	figName = [figNamePrefix, figs{iid}];
-	%% Load data {{{
-	addpath([projPath, '/PostProcessing/']);
-	[folderList, nameList] = getFolderList(Id, 0);
-
-	% Load simulations from transient.mat
-	transData = loadData(folderList, 'levelset', [projPath, 'Models/']);
-	Ntrans = length(transData);
-	%}}}
-	%% Average behaviors {{{
-	for i = 1: 1
-		figure('position',[0,1000,800,1000])
-		for tid = 1:length(tList)
-
-
-		end
-		%	subplot(nsub, 1, 1);
-		%	title('total misfit area')
-		%	xlim([0, finalTime])
-		%	ylim([-25, 25])
-		%	xlabel('Time (a)')
-		%	ylabel('Misfit area (km$^2$)', 'Interpreter', 'latex')
-
-		%	subplot(nsub, 1, 2);
-		title(figtitles{iid}, 'Interpreter', 'latex')
-		xlim([1, finalTime])
-		if contains(figtitles{iid}, '5000')
-			ylim([0.02, 80])
-		else
-			%ylim([0, 45])
-			ylim([0.02, 80])
-		end
-		xlabel('Time (a)', 'Interpreter', 'latex')
-		ylabel('Absolute misfit area (km$^2$)', 'Interpreter', 'latex')
-
-		%	legend(nameList, 'Interpreter', 'latex','Orientation','vertical','Location','eastoutside')
+		figName = [figNamePrefix, 'errors_st', num2str(st), '_re', num2str(re), '_t', num2str(transData{i}.time(tid))];
 		set(gcf,'color','w');
 		if saveflag
+			disp(['  Save to ', figName])
 			export_fig([figName, '.pdf'])
 		end
-		%}}}
-	end %}}}
+	end
 end
+%}}}
