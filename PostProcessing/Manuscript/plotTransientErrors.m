342e7fdf9b9a4ceb4860888f82fd3490736f2622
Cheng Gong <enigne@gmail.com>
2024-06-27 11:28:25 -0400

    generate figures

--- PostProcessing/Manuscript/plotTransientErrors.m
+++ PostProcessing/Manuscript/plotTransientErrors.m
@@ -3,25 +3,26 @@ close all
 addpath('../')
 glacier = 'Levelset';
 projPath = ['/totten_1/chenggong/', glacier, '/'];
-figNamePrefix = [projPath, 'PostProcessing/Manuscript/Figures/structured_mesh/refine25/'];
-saveflag = 0;
+saveflag = 1;
 stList = [1];%,2,5,6];
 reList = [1];%100
+%folderList = {['20240309_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
+%folderList = {['20231219_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
+%folderList = {['20230307_LS_rectangle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
+%folderList = {['20230227_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
+prefix = '20240624_LS_circle_uniform_vx1000';
+figNamePrefix = [projPath, 'PostProcessing/Manuscript/Figures/structured_mesh/refine50/'];
 
+% load model
+md = loadRefMd([projPath, 'Models/', prefix, '_stab1_reinit1'], 'Transient');
 
 for ist = 1:numel(stList)
 	for ire = 1:numel(reList)
 		st = stList(ist); 
 		re = reList(ire);
 
-		folderList = {['20240626_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
-		%folderList = {['20240309_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
-		%folderList = {['20231219_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
-		%folderList = {['20230307_LS_rectangle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
-		%folderList = {['20230227_LS_circle_uniform_vx1000_stab', num2str(st), '_reinit', num2str(re), '/']};
+		folderList = {[prefix, '_stab', num2str(st), '_reinit', num2str(re), '/']};
 		% Load data {{{
-		% load model
-		md = loadRefMd();
 		% load data
 		transData = loadData(folderList, 'levelset', [projPath, 'Models/']);
 		NT = transData{1}.NT;
@@ -37,6 +38,7 @@ for ist = 1:numel(stList)
 				plotmodel(md, 'data', dist, 'figure', p, 'caxis', [-1,1], 'title', ['time=', num2str(transData{i}.time(tid))], 'figposition', [0,1000,500,400])
 				xlabel('x (m)')
 				ylabel('y (m)')
+				colormap(jet)
 
 				figName = [figNamePrefix, 'errors_st', num2str(st), '_re', num2str(re), '_t', replace(num2str(transData{i}.time(tid)),'.','_')];
 				set(gcf,'color','w');
